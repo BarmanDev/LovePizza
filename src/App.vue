5e04f295fbc67dfb980d1d9f440535e316f97d37
Borja <elbarmandev@gmail.com>
2023-12-02 14:14:19 +0100

    hader finish

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from './components/HeaderComponent.vue';
+</script>
+
+<template>
+  <HeaderComponent/>
+  <RouterView />
+</template>
+
+<style>
+
+
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,65 @@
+<template>
+  <header>
+    <nav>
+      <div class="logo-container">
+        <img class="img-logo" src="../assets/favicon.png" alt="Logo de la Pizzería">
+      </div>
+      <ul>
+        <li><router-link to="/" class="nav-link">Inicio</router-link></li>
+        <li><router-link to="/about" class="nav-link">Pedidos</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+});
+</script>
+
+<style scoped>
+nav {
+  height: 120px;
+  background-color: var(--color-green);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+}
+
+.logo-container {
+  display: flex;
+  align-items: center;
+}
+
+.img-logo {
+  max-width: 100px; 
+
+}
+
+ul {
+  list-style: none;
+  display: flex;
+  gap: 20px;
+}
+
+li {
+  font-size: 1.5rem; 
+  font-family: Arial, Helvetica, sans-serif;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+.nav-link {
+  color: var( --color-red-pomodoro); 
+  text-decoration: none; 
+  transition: color 0.3s ease;
+}
+
+.nav-link:hover {
+  color: #fff; 
+}
+</style>
